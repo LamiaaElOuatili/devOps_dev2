0dcefdf1042aa15d2ac7ae912ba211e1e78518e6
Lamiaa El Ouatili <lamiaaelouatili@hotmail.com>
2024-04-24 09:52:45 +0100

    first commit

---
+++ src/main/java/com/example/dev2_devops/Dev2DevOpsApplication.java
@@ -0,0 +1,13 @@
+package com.example.dev2_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dev2DevOpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dev2DevOpsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dev2_devops/Dev2DevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dev2_devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dev2DevOpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
